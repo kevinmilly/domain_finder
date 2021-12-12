633bed7fdfce0bd99d2ae23386ac748d88f72598
kevinmilly <kevinmilly@gmail.com>
2021-12-12 09:43:55 -0500

    changes to logic and csv export

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
-const GoDaddy = require('godaddy-api');
-const {words} = require('./words');
+const GoDaddy = require("godaddy-api");
+const { words } = require("./words");
+require("dotenv").config();
+const createCSVWriter = require("csv-writer").createObjectCsvWriter;
+const csv = require("csv-parser");
+const fs = require("fs");
 
-const API_KEY='9EE9DGxPFuw_A8BVJNT25YhHhE1M1i4uZh'
-const SECRET='Ren6vwWitBXfSs2aguknZZ'
+godaddy = GoDaddy(process.env.API_KEY, process.env.API_SECRET);
 
-godaddy = GoDaddy(API_KEY,SECRET);
+const domainWriter = createCSVWriter({
+  path: "availability-results/domainAvailability.csv",
+  header: [
+    { id: "domainName", title: "Domain Name" },
+    { id: "availability", title: "Availability" },
+  ],
+});
 
-const request = `https://api.ote-godaddy.com/v1/domains/available?domain=`
-
-
-const isApplicable = (available, price) => price < 20 && available;
-
-const searchDomain = () => {
-    words.forEach(async (word, index) => {
-        index % 60 ===0 && await sleep(61000);
-        try {
-              const response = await godaddy.domains.available({domain:`${word}.com`});
-              const {available, domain} = response.body;
-              console.log(`Trying ${domain}`);
-              available && console.log(`${domain} is available!`);
-        } catch (error) {
-            console.log({error})   ;
-        }
-    });
-    
-}
-
-
-function sleep(ms) {
-  return new Promise((resolve) => {
+const sleep = (ms) => {
+  console.log(`Sleeping for ${ms/1000} seconds`);
+  return  new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
+const resultsToExport = [];
+const wordsToTry = [];
+let wordsTried = 0;
+const searchDomain = () => {
+  fs.createReadStream("source_data/words_to_search.csv")
+    .pipe(csv())
+    .on("data", async ({ name }) => {
+      const formattedName = `${name && name.trim()}.com`;
+      !!formattedName && wordsToTry.push(formattedName);
+    })
+    .on("end", async () => {
+      console.log("CSV file successfully processed");
+      wordsToTry.forEach(async (word) => {
+        ++wordsTried % 60 === 0 && await sleep(61000);
+        try {
+          const response = await godaddy.domains.available({
+            domain: `${word}`,
+          });
+          await sleep(2000);
+          const { available, domain } = response.body;
+           console.log(`Trying Godaddy api for #${wordsTried}: ${domain}`);
+          domain && resultsToExport.push({ domainName: domain, availability: available });
+          available
+            ? console.log(`${domain} is available!`)
+            : console.log(`No for ${domain}`);
+          console.log(`Results size is now ${resultsToExport.length}`)
+        } catch (error) {
+          console.log({ error });
+          await domainWriter.writeRecords(resultsToExport);
+        }
+      });
+    });
+};
+
 searchDomain();
-
-
-
